ed64cbb813b52cae9db8d61d7da88db876611386
Brit Butler <redline6561@gmail.com>
2015-07-22 17:11:00 -0400

    Add avatar_url to user model.

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20150721190450) do
+ActiveRecord::Schema.define(version: 20150722211039) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -47,6 +47,7 @@ ActiveRecord::Schema.define(version: 20150721190450) do
     t.string   "state"
     t.string   "country"
     t.integer  "soundcloud_id"
+    t.string   "avatar_url"
   end
 
   add_index "users", ["access_token"], name: "index_users_on_access_token", unique: true, using: :btree
