7ddf9fffa37e318288e4d442c443b82818b685a8
Brit Butler <redline6561@gmail.com>
2015-07-22 16:32:32 -0400

    Fix track import job.

--- app/jobs/track_import_job.rb
+++ app/jobs/track_import_job.rb
@@ -9,7 +9,7 @@ class TrackImportJob < ActiveJob::Base
       logger.error "Error: #{e.message}, Status Code: #{e.response.code}"
       if e.response.code == 401
         api = SoundcloudApi.new(access_token: user.soundcloud_token,
-                                refresh_token: user.refresh_token)
+                                refresh_token: user.refresh_token).api
         api.get('/me') # This should update the access token
         if api.access_token != user.soundcloud_token
           user.update(soundcloud_token: api.access_token,
