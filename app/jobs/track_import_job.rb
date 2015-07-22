a2075415e60f54baba462605baa4d734394e2bdd
Brit Butler <redline6561@gmail.com>
2015-07-22 12:22:58 -0400

    Handle refresh token logic in TrackImportJob.

--- app/jobs/track_import_job.rb
+++ app/jobs/track_import_job.rb
@@ -2,7 +2,21 @@ class TrackImportJob < ActiveJob::Base
   queue_as :default
 
   def perform(user)
-    @importer = TrackImporter.new(user)
-    @importer.run!
+    begin
+      @importer = TrackImporter.new(user)
+      @importer.run!
+    rescue SoundCloud::ResponseError => e
+      logger.error "Error: #{e.message}, Status Code: #{e.response.code}"
+      if e.response.code == 401
+        api = SoundcloudApi.new(access_token: user.soundcloud_token,
+                                refresh_token: user.refresh_token)
+        api.get('/me') # This should update the access token
+        if api.access_token != user.soundcloud_token
+          user.update(soundcloud_token: api.access_token,
+                      refresh_token: api.refresh_token)
+        end
+        TrackImportJob.perform_later(user)
+      end
+    end
   end
 end
