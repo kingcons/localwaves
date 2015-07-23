093881bda9306f211c1e2018bcbe1138f579f688
Brit Butler <redline6561@gmail.com>
2015-07-23 09:05:22 -0400

    Delete a User's tracks when the user is deleted.

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,7 +1,7 @@
 class User < ActiveRecord::Base
   EMAIL_REGEX = /\A([^@\s]+)@((?:[-a-z0-9]+\.)+[a-z]{2,})\z/i
 
-  has_many :tracks
+  has_many :tracks, dependent: :destroy
 
   validates_presence_of :email, :password_digest, :access_token
   validates_uniqueness_of :email
